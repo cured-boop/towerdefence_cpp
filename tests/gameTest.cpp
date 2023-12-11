1adf0ecc2bc6f104ff8a64d0549f8fcff3728349
Tiusanen Antti <antti.tiusanen@aalto.fi>
2023-12-11 18:14:29 +0200

    Documentation

---
+++ tests/gameTest.cpp
@@ -0,0 +1,19 @@
+/*
+Tested manually during GUI runtime. All of the elements included in the Game
+contain separate testing and thus are not included here.
+1. 
+Should load the correct level without issues.
+Works appropriately.
+2.
+Should handle user events and effects on the game.
+Works appropriately.
+3.
+Should handle passing of time by updating enemies and towers.
+Works approrpiately.
+4. 
+Should result in a game loss screen when the game is lost.
+Works appropriately. 
+5.
+Should draw everything on correct positions all the time.
+Works appropriately.
+*/
---
+++ tests/mainTest.cpp
@@ -0,0 +1,19 @@
+/*
+Tested manually by running the main program.
+1.
+The program should boot and close without issues.
+Works appropriately. 
+2.
+Drawing all of the GUI elements should work.
+Works appropriately.
+3. 
+Main menu should work as intended and start a game instance
+with "Start" button.
+Works appropriately. 
+4.
+Should react to user input.
+Works appropriately.
+5.
+Loading all configurations should work properly.
+Works appropriately.
+*/
